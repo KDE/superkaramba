367eddf6438aef44ce73b688fbc6a85449619e8b
Laurent Montel <montel@kde.org>
2006-07-03 12:42:20 +0000

    Include fix with new qtdbus
    svn path=/trunk/KDE/kdeutils/superkaramba/; revision=557509

--- src/superkarambaadaptor.h
+++ src/superkarambaadaptor.h
@@ -13,7 +13,7 @@
 #define SUPERKARAMBAADAPTOR_H_139301150805079
 
 #include <QtCore/QObject>
-#include <dbus/qdbus.h>
+#include <QtDBus>
 class QByteArray;
 template<class T> class QList;
 template<class Key, class Value> class QMap;
