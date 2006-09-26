ba6ebcda1d709a547825570616bb1aa5e2c104ac
Alexander Wiedenbruch <wirr01@gmail.com>
2006-09-26 20:07:27 +0000

    visibleName -> visibleName()
    svn path=/branches/work/sk4/porting/superkaramba/; revision=588760

--- src/taskmanager.cpp
+++ src/taskmanager.cpp
@@ -475,9 +475,7 @@ bool Task::isOnTop() const
 bool Task::isModified() const
 {
   static QString modStr = QString::fromUtf8("[") + i18n("modified") + QString::fromUtf8("]");
-  int modStrPos = _info.visibleName.contains(modStr);
-
-  return ( modStrPos != -1 );
+  return _info.visibleName().contains(modStr);
 }
 
 QString Task::iconName() const
