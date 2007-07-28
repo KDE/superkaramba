f4fb759f42b60267720944d6baa0994cdd19bd6e
Sebastian Sauer <mail@dipe.org>
2007-07-28 19:40:41 +0000

    the Plasma::TimeEngine sample at http://techbase.kde.org/Development/Tutorials/SuperKaramba#Access_Plasma just works now :)
    svn path=/trunk/KDE/kdeutils/superkaramba/; revision=693715

--- skapplet/skengineadaptor.h
+++ skapplet/skengineadaptor.h
@@ -22,6 +22,7 @@
 #define SKENGINEADAPTOR_H
 
 #include <QObject>
+//#include <QMetaObject>
 #include <QVariant>
 #include <plasma/dataengine.h>
 
@@ -36,8 +37,26 @@ class SuperKarambaEngineAdaptor : public QObject
         explicit SuperKarambaEngineAdaptor(Plasma::DataEngine* engine) : QObject(), m_engine(engine) {
             connect(m_engine, SIGNAL(newSource(QString)), this, SIGNAL(sourceAdded(QString)));
             connect(m_engine, SIGNAL(sourceRemoved(QString)), this, SIGNAL(sourceRemoved(QString)));
+            //m_engine->setProperty("reportSeconds", true);
         }
         virtual ~SuperKarambaEngineAdaptor() {}
+
+        QVariantMap dataToMap(Plasma::DataEngine::Data data) {
+            QVariantMap map;
+            Plasma::DataEngine::DataIterator it(data);
+            while( it.hasNext() ) {
+                it.next();
+                map.insert(it.key(), it.value());
+            }
+            return map;
+        }
+
+        Plasma::DataEngine::Data mapToData(QVariantMap map) {
+            Plasma::DataEngine::Data data;
+            for(QVariantMap::Iterator it = map.begin(); it != map.end(); ++it)
+                data.insert(it.key(), it.value());
+            return data;
+        }
 
     public Q_SLOTS:
 
@@ -47,18 +66,40 @@ class SuperKarambaEngineAdaptor : public QObject
         QStringList sources() const { return m_engine->sources(); }
 
         /**
+        * Get a property.
+        */
+        QVariant property(const QByteArray& name) const { return m_engine->property(name); }
+
+        /**
+        * Set a property.
+        */
+        void setProperty(const QByteArray& name, const QVariant& value) { m_engine->setProperty(name, value); }
+
+        /**
         * Gets the data associated with a data source.
         */
-        QVariantMap query(const QString& source) const {
-            QVariantMap map;
-            Plasma::DataEngine::Data data = m_engine->query(source);
-            Plasma::DataEngine::DataIterator it(data);
-            while( it.hasNext() ) {
-                it.next();
-                map.insert(it.key(), it.value());
-            }
-            return map;
+        QVariantMap query(const QString& source) { return dataToMap(m_engine->query(source)); }
+
+        /**
+        * Connect with a source. Each time the source is updated
+        * the sourceUpdated() signal will be emitted.
+        */
+        void connectSource(const QString& source) { m_engine->connectSource(source, this); }
+
+        /**
+        * Disconnect from a source.
+        */
+        void disconnectSource(const QString& source) { m_engine->disconnectSource(source, this); }
+
+#if 0
+        /**
+        * Request to update the source.
+        */
+        void updateSource(const QString& source, QVariantMap data = QVariantMap()) {
+            //QMetaObject::invokeMethod(m_engine, "checkForUpdates");
+            updated(source, mapToData(data));
         }
+#endif
 
     Q_SIGNALS:
 
@@ -71,6 +112,18 @@ class SuperKarambaEngineAdaptor : public QObject
          * Emitted when a data source is removed.
          **/
         void sourceRemoved(const QString& source);
+
+        /**
+         * Emitted when a data source is removed.
+         **/
+        void sourceUpdated(const QString& source, QVariantMap data);
+
+    private Q_SLOTS:
+
+        void updated(const QString& source, Plasma::DataEngine::Data data) {
+            //kDebug()<<"SuperKarambaEngineAdaptor::updated source="<<source<<endl;
+            emit sourceUpdated(source, dataToMap(data));
+        }
 
     private:
         Plasma::DataEngine* m_engine;
