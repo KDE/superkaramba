dc82c8937f2ed6d3b64324dc65d84d1ea7efdaca
Arto Hytönen <amthpublic@gmail.com>
2007-09-11 07:14:05 +0000

    SVN_SILENT style fixes
    svn path=/trunk/KDE/kdeutils/superkaramba/; revision=711001

--- src/meters/imagelabel.cpp
+++ src/meters/imagelabel.cpp
@@ -24,10 +24,11 @@
 #include "imagelabel.h"
 #include "karambaapp.h"
 
-#include <QPixmap>
-#include <QToolTip>
-#include <QCursor>
-#include <QBitmap>
+#include <QtGui/QPixmap>
+#include <QtGui/QToolTip>
+#include <QtGui/QCursor>
+#include <QtGui/QBitmap>
+#include <QtGui/QImage>
 
 #include <KPixmapEffect>
 #include <KIconEffect>
@@ -105,7 +106,7 @@ ToGray::ToGray(ImageLabel* img, int millisec) : Effect(img, millisec)
 
 QPixmap ToGray::apply(QPixmap pixmap)
 {
-    QImage image = pixmap.toImage();
+    QImage image = pixmap().toImage();
     KIconEffect::toGray(image, 1.0f); // maybe Blitz::grayscale() would be better
     return pixmap = QPixmap::fromImage(image);
 }
@@ -260,7 +261,7 @@ void ImageLabel::applyTransformations(bool useSmoothScale)
             pixmap = pm;
         } else {
             if (useSmoothScale) {
-                pixmap = QPixmap::fromImage(pixmap.toImage().scaled(scale_w, scale_h, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+                pixmap = QPixmap::fromImage(pixmap().toImage().scaled(scale_w, scale_h, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
             } else {
                 double widthFactor = ((double)scale_w) / ((double)pixmap.width());
                 double heightFactor = ((double)scale_h) / ((double)pixmap.height());
@@ -384,7 +385,7 @@ void ImageLabel::paint(QPainter *painter, const QStyleOptionGraphicsItem *option
             painter->drawPixmap(0, 0, pixmap);
         } else {
 	    //Blend this image with a color
-	    QImage image = pixmap.toImage();
+	    QImage image = pixmap().toImage();
 	    QPainter p;
 
 	    p.begin(&image);
