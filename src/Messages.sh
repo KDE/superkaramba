9aea05b1decc28dbbe888808a5edefa83b6f97bc
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:36:58 +0000

    fix Messages.sh after yesterday change in extract-messages.sh
    svn path=/trunk/KDE/kdeutils/superkaramba/; revision=1166692

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 ### TEMPORARY for KDE4, might extract too much
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.ui -o -name \*.kcfg -o -name \*.rc` >> rc.cpp || exit 11
 $XGETTEXT -kaliasLocal *.cpp -o $podir/superkaramba.pot
